daf0e776c1cb852bddc0510c2ecf8431f29b49d7
Dell <Dell@MacBook-Air.local>
2026-07-23 18:56:32 +0530

    fix: hardcode exact Atlas connection string in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,15 @@
 const mongoose = require("mongoose");
 const dns = require("dns");
 
-// Set public DNS servers to resolve MongoDB SRV records reliably
 try {
   dns.setServers(["8.8.8.8", "1.1.1.1"]);
-} catch (e) {
-  console.log("DNS setServers warning:", e.message);
-}
+} catch (e) {}
 
 /**
  * Connect to MongoDB
  */
 const connectDB = async () => {
-  const uri =
-    process.env.MONGO_URI ||
-    "mongodb+srv://omt898468_db_user:ovOHe8c2CLqBKCVv@cluster0.addvkgc.mongodb.net/leaveManagementDB?retryWrites=true&w=majority";
+  const uri = "mongodb+srv://omt898468_db_user:ovOHe8c2CLqBKCVv@cluster0.addvkgc.mongodb.net/leaveManagementDB?retryWrites=true&w=majority";
 
   try {
     const conn = await mongoose.connect(uri);
